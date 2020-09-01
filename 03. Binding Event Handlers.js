afd8248cf58fcc6e71e240423747758223fab500
Atul Malakar <atulmalakar15@gmail.com>
2020-09-02 03:04:50 +0530

    Create 03. Binding Event Handlers.js
    Using arrow functions solved issues and is the best convention to add event handlers !

---
+++ 03. Binding Event Handlers.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count : 0
+        }
+        //this.increment = this.increment.bind(this);
+    }
+    
+    increment = () => {
+            console.log(this);
+            this.setState({
+                count : this.state.count + 1
+            })
+        }
+    
+
+    render(){
+        return (
+            <div>
+                <h1>Counter is displayed below !</h1>
+                <h1>{this.state.count}</h1>
+                {/*<button onClick = {() => this.increment()}>Increment</button>*/}
+
+                <button onClick = {this.increment}>Increment</button>
+
+            </div>
+        )
+    }
+};
+
+export default Counter;
